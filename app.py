0d6e51ecd51f2bf82b95ebb8740166af3856ebf5
Barodun <alex.baradun@gmail.com>
2023-11-28 20:25:16 +0300

    modified:   app.py modified:   gpt_generator.py

--- app.py
+++ app.py
@@ -35,33 +35,34 @@ def generate():
 
 @app.route('/', methods=['GET'])
 def default():
-    # prompt = 'Translate this text to English. There is the text:'
-    # content = 'There is the simple text to translate'
-    # api_key = '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'
-    # gpt_model = 'gpt-3.5-turbo'
-    # res = gpt_generator.generate_text(prompt, content, api_key, gpt_model)
-    # print(res)
-    # return res
+    # wp_url = 'https://staging3.staging2.blockchainspost.com'
+    # post_id = 570  # ID of the post to update
+    # username = 'generator'
+    # app_pass = 'hpqJ CoHf Vugg xSv7 MRyp ZD0L'
+    # # Data you want to update
     # data = {
     #     'title': 'New Post Title',
-    #     'content': 'This is the updated content of the post.'
+    #     'content': 'Updated content here',
+    #     'meta': { '_yoast_wpseo_metadesc': 'New Yoast meta desc' }
     # }
-    # post_id = '565'
-    # username = 'generator@pansevich.me'
-    # password = 'J%*ymb9x48%cXTK6mBMzU7#t'
-    # response = gpt_generator._change_post_data(post_id, data, username, password)
-    # return response
-    wp_url = 'https://staging3.staging2.blockchainspost.com'
-    post_id = 570  # ID of the post to update
-    username = 'generator'
-    app_pass = 'hpqJ CoHf Vugg xSv7 MRyp ZD0L'
-    # Data you want to update
-    data = {
-        'title': 'New Post Title',
-        'content': 'Updated content here',
-        'meta': { '_yoast_wpseo_metadesc': 'New Yoast meta desc' }
-    }
-    return gpt_generator._change_post_data(wp_url, post_id, username, app_pass, data)
+    # return gpt_generator._change_post_data(wp_url, post_id, username, app_pass, data)
+    # return gpt_generator.generate_text()
+    response = '1. <title>Bitcoin price increased by more than $2k in the past 24 hours</title>2. <content><p><img title="Bitcoin price increased by more than $2k in the past 24 hours" src="/wp-content/uploads/2023/11/ef15dfdfb2283ed490cf19d59ac53d6b.jpg" alt="Bitcoin price increased by more than $2k in the past 24 hours" /></p> <p>The entire top ten largest cryptocurrencies demonstrated positive dynamics over the past 24 hours</p> <p dir="ltr">The price of Bitcoin (BTC) increased by more than 5% from November 15th to 16th, reaching $37,890 against Tether USD (USDT) on the Binance exchange. As of 10:00 Moscow time, BTC is trading at $37,570.</p> <h2>BTC/USD</h2> <p>37,538 +1,879 (5.27%) Nov 16 10:39:14 1d 3d 1m 3m 1y 5y</p> <p dir="ltr">Price fluctuations in cryptocurrencies have led to a mass liquidation of traders margin positions on cryptocurrency exchanges. According to Coinglass, over the past 24 hours, more than 51,000 traders collectively lost over $155 million, with the largest liquidation amounting to $2.93 million on the Binance exchange.</p> <p dir="ltr">If the quotes go in the opposite direction to what the trader expected in margin trading, they start incurring losses. Losses reduce the margin size, and when it decreases to a critical value, the trader receives a notification from the broker to replenish the account. This notification is called a Margin Call. If the account is not replenished and the losses continue to grow, the broker will automatically close the positions opened by the trader.</p></content>3. <desc>Bitcoin price increased by more than $2k in the past 24 hours. The top ten largest cryptocurrencies demonstrated positive dynamics. BTC/USD: 37,538 +1,879 (5.27%). Price fluctuations led to a mass liquidation of traders margin positions, resulting in over $155 million in losses. Margin Call is issued when losses exceed the critical value.</desc>'
+    title_idx_start = response.index('<title>')
+    title_idx_end = response.index('</title>')
+    title = response[title_idx_start + len('<title>'): title_idx_end]
+
+    content_idx_start = response.index('<content>')
+    content_idx_end = response.index('</content>')
+    content = response[content_idx_start + len('<content>'): content_idx_end]
+
+    desc_idx_start = response.index('<desc>')
+    desc_idx_end = response.index('</desc>')
+    desc = response[desc_idx_start + len('<desc>'): desc_idx_end]
+
+    
+    return desc
+
 
 if __name__ == '__main__':
     app.run()
--- gpt_generator.py
+++ gpt_generator.py
@@ -21,32 +21,10 @@ class GptGenerator(object):
             print("Response:", response.text)
         return response.json()
 
-    def chat_with_gpt(self, prompt, content, api_key, model):
+    def _generate_content(self, prompt, content, api_key, model):
         openai.api_key = api_key
         text_for_gpt = prompt + content
-        content_len = int(len(content) / 3)
-        try:
-            response = openai.ChatCompletion.create(
-                model=model,
-                messages=[
-                    # {"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": text_for_gpt}
-                ],
-                temperature=0,
-                max_tokens=content_len,
-                top_p=1,
-                frequency_penalty=0.01,
-                presence_penalty=0.01,
-                stop=["<!--noindex-->"]
-            )
-            return response.choices[0].message['content']
-        except Exception as e:
-            return str(e)
-
-    def generate_text(self, prompt, content, api_key, model, wp_url, post_id, username, app_pass):
-        openai.api_key = api_key
-        text_for_gpt = prompt + content
-        content_len = int(len(content) / 3)
+        content_len = int(len(text_for_gpt) / 3)
         try:
             response = openai.ChatCompletion.create(
                 model=model,
@@ -62,14 +40,30 @@ class GptGenerator(object):
                 stop=["<!--noindex-->"]
             )
             new_content = response.choices[0].message['content']
-            # return response.choices[0].message['content']
+            return response.choices[0].message['content']
         except Exception as e:
             print(str(e))
             return ['Error', str(e)]
 
+    def generate_text(self, prompt, content, api_key, model, wp_url, post_id, username, app_pass):
+        # new_content = self._generate_with_gpt( prompt, content, api_key, model)
+        response = self._generate_content(prompt, content, api_key, model)
+        title_idx_start = response.index('<title>')
+        title_idx_end = response.index('</title>')
+        title = response[title_idx_start + len('<title>'): title_idx_end]
+
+        content_idx_start = response.index('<content>')
+        content_idx_end = response.index('</content>')
+        content = response[content_idx_start + len('<content>'): content_idx_end]
+
+        desc_idx_start = response.index('<desc>')
+        desc_idx_end = response.index('</desc>')
+        desc = response[desc_idx_start + len('<desc>'): desc_idx_end]
+
         data = {
-            'title': 'New Post Title',
-            'content': new_content,
-            'meta': { '_yoast_wpseo_metadesc': 'New Yoast meta desc' }
+            'title': title,
+            'content': content,
+            'meta': { '_yoast_wpseo_metadesc': desc }
         }
+
         return self._change_post_data(wp_url, post_id, username, app_pass, data)
